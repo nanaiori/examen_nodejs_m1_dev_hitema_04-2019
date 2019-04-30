6db7a90e476f8e4af5de79eecc50a715305d61be
nanaiori <eljaafarii.Najlaa@gmail.com>
2019-04-30 16:44:51 +0200

    mon implementation

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,20 @@
 const crypto = require('crypto');
+const HttpStatus = require('http-status-codes');
 
 function sha1Encode(data) {
     // To be implemented!
+    const hash = crypto.createHash('sha1');
+    hash.update(data);
+   return hash.digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
     // To be implemented!
+    const authorization = request.headers.authorization;
+    const code_pass = sha1Encode('password');
+    const encoded = authorization.replace('Basic ', '');
+    const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+    const [login, password] = decoded.split(':');
+    if (login == 'node' && password == code_pass) return next();
+    response.sendStatus(HttpStatus.UNAUTHORIZED);
 }
---
+++ index.js
@@ -0,0 +1,13 @@
+/*const app  = require('./main.js');
+const express = require('express');
+require('./web/websocket-server');
+app.use(express.static('public'));
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server listening on port:', process.env.APP_PORT);
+})*/
+
+/*
+const bodyParser = require('body-parser');
+const app = express();
+const v1 = express.Router();
+const { basicAuth } = require('./basic-auth');*/
