33a687bde095cadb77e258d15723befc1d406ee8
nanaiori <eljaafarii.Najlaa@gmail.com>
2019-04-30 17:09:43 +0200

    mon implementation

--- io/io.js
+++ io/io.js
@@ -1,12 +1,13 @@
-module.exports.decodeHexFileContent = (filePath) =>  {
-    var fs = require('fs');
-        var readStream = fs.createReadStream('./io/input');
-        readStream.pipe(process.stdout);
-    return new Promise((resolve, reject) => {
-       
-        if (filePath === null) 
-        resolve(filePath);
-        else reject(null);
+const fs = require('fs');
 
+
+module.exports.decodeHexFileContent = (filePath) =>  {
+    return new Promise((resolve, reject) => {
+
+
+        var content = fs.readFileSync(filePath, 'utf8');
+        content = Buffer.from(content, 'hex').toString('utf8');
+        if(content)
+            resolve(content)
     });
 }
